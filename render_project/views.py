5365193d52b59444262ccccccdbb74cd62a4760f
Shalim Hassan <salim.hassan219@gmail.com>
2026-04-15 17:14:32 +0600

    data add to views

--- render_project/views.py
+++ render_project/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home_page(request):
-  return render(request,"home.html")
+  
+  info ={
+    "name": "Rohim",
+    "address": "Korimnagar",
+  }
+
+  return render(request,"home.html", info)
